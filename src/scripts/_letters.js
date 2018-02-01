42ca11f3576ab6c687fae90c2f791e1c41959fd2
Rebecca Reed <rebeccareed@Rebeccas-MacBook-Pro.local>
2018-02-01 16:58:05 +0000

    Work on animation

--- src/scripts/_letters.js
+++ src/scripts/_letters.js
@@ -26,10 +26,43 @@ export default class Letters {
     }
     
     showLetters() {
+        // document.querySelectorAll(this.selectors.letter).forEach((letter, index) => {
+        //     setTimeout(() => {
+        //         letter.classList.add(this.classes.visible);
+        //     }, index * 100);
+        // });
+
+
+        let promise = Promise.resolve();
+
         document.querySelectorAll(this.selectors.letter).forEach((letter, index) => {
-            setTimeout(() => {
+            promise = promise.then(() => {
                 letter.classList.add(this.classes.visible);
-            }, index * 100);
+                return new Promise((resolve) => {
+                  setTimeout(resolve, index * 100);
+                });
+            });
+        });
+
+        promise.then(function () {
+            console.log('Loop finished.');
         });
     }
 }
+
+
+// var array = ['some', 'array', 'containing', 'words'];
+// var interval = 1000; // how much time should the delay between two iterations be (in milliseconds)?
+// var promise = Promise.resolve();
+// array.forEach(function (el) {
+//   promise = promise.then(function () {
+//     console.log(el);
+//     return new Promise(function (resolve) {
+//       setTimeout(resolve, interval);
+//     });
+//   });
+// });
+
+// promise.then(function () {
+//   console.log('Loop finished.');
+// });
